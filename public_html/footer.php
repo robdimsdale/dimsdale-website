d403553ec166259334c6277e087d0e51e32c25d0
robdimsdale <robert.dimsdale@gmail.com>
2014-04-15 03:32:42 -0600

    Shorten Font Awesome line to fit neatly on narrow devices.

--- public_html/footer.php
+++ public_html/footer.php
@@ -13,7 +13,7 @@
             <p class="text-muted">&copy; 2014 Rob Dimsdale</p>
           </div>
           <div class="col-xs-12 col-sm-8 col-md-6">
-            <p class="text-muted credit">Font Awesome by Dave Gandy - <a href="http://fontawesome.io">http://fontawesome.io</a></p>
+            <p class="text-muted credit">Font Awesome - <a href="http://fontawesome.io">http://fontawesome.io</a></p>
           </div>
         </div>
       </div>
