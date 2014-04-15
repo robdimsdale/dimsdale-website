58c03a9538f4b8e5cb39fc098c2a4fc339ac6d2a
robdimsdale <robert.dimsdale@gmail.com>
2014-04-15 04:59:20 +0100

    Change title to 'Rob Dimsdale'.

--- public_html/header-navbar.php
+++ public_html/header-navbar.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dimsdale</title>
+  <title>Rob Dimsdale</title>
 
   <script type="text/javascript" src="/static/js/jquery-2.1.0.min.js"></script>
 
