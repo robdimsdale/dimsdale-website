03a720d434a6fd985bbadb55b4b77b59d71ed138
robdimsdale <robert.dimsdale@gmail.com>
2014-04-14 18:43:55 -0600

    Convert html to php.

---
+++ public_html/header-navbar.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dimsdale</title>
+  <script type="text/javascript" src="/static/js/jquery-2.1.0.min.js"></script>
+  <link rel="stylesheet" href="/static/css/bootstrap.min.css" type="text/css" />
+  <script type="text/javascript" src="/static/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" href="/static/css/application.css" type="text/css" />
+</head>
+
+<body>
+  <div id="header">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="row">
+          <div class="col-md-12">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <h2><a href="/">dimsdale.net</a></h2>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="/about-me/">About Me</a></li>
+                <li><a href="/resume/">Resume</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.col-md-12 -->
+        </div><!-- /.row -->
+      </div>
+    </nav>
+  </div>
+
---
+++ public_html/index.php
@@ -0,0 +1,43 @@
+<?php require('header-navbar.php'); ?>
+  <div class="container page">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Rob Dimsdale</h1>
+        <p class="bio-brief">Polyglot developer and occasional system administrator</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-3 col-xs-offset-0 col-sm-2 col-sm-offset-2 col-md-1 col-md-offset-4">
+        <a href="http://github.com/robdimsdale">github</a>
+      </div>
+      <div class="col-xs-3 col-sm-2 col-md-1">
+        <a href="http://bitbucket.com/robdimsdale">bitbucket</a>
+      </div>
+      <div class="col-xs-3 col-sm-2 col-md-1">
+        <a href="https://www.linkedin.com/profile/view?id=177587965">linkedin</a>
+      </div>
+      <div class="col-xs-3 col-sm-2 col-md-1">
+        <a href="http://another.com">another</a>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12">
+        <hr/>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12">
+        <a href="https://recipes.dimsdale.net">recipes (secure)</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <a href="https://jenkins.dimsdale.net">jenkins (secure)</a>
+      </div>
+    </div>
+
+  </div>
+<? require('footer.php'); ?>
