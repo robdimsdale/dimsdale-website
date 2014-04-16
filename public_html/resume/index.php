fed543d69241b6c6a1759a44f31d3751ed355508
robdimsdale <robert.dimsdale@gmail.com>
2014-04-16 00:45:21 -0600

    Add link to robdimsdale-resume.pdf.

--- public_html/resume/index.php
+++ public_html/resume/index.php
@@ -6,4 +6,10 @@
     </div>
   </div>
 
+  <div class="row">
+    <div class="col-xs-12 col-md-8 col-md-offset-2">
+      <a href="robdimsdale-resume.pdf">robdimsdale-resume.pdf</a>
+    </div>
+  </div>
+
 <?php require('../footer.php'); ?>
