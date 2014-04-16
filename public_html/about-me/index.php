a8f5a5f378ad447d06a034007c63eb033624cee5
robdimsdale <robert.dimsdale@gmail.com>
2014-04-16 14:40:24 -0600

    About me gets real information.

--- public_html/about-me/index.php
+++ public_html/about-me/index.php
@@ -1,20 +1,21 @@
 <?php require('../header-navbar.php'); ?>
 
-  <div class="row">
-    <div class="col-md-12">
-      <h1>About me</h1>
-    </div>
+<div class="row">
+  <div class="col-md-12">
+    <h1>About me</h1>
   </div>
+</div>
 
-  <div class="row">
-    <div class="col-xs-12 col-md-8 col-md-offset-2">
-      <p>Polyglot developer and occasional system administrator. Polyglot developer and occasional system administrator</p>
-      <p>Polyglot developer and occasional system administrator. Polyglot developer and occasional system administrator</p>
-      <p>Polyglot developer and occasional system administrator. Polyglot developer and occasional system administrator. Polyglot developer and occasional system administrator</p>
-      <p>Polyglot developer and occasional system administrator. Polyglot developer and occasional system administrator. Polyglot developer and occasional system administrator</p>
-      <p>Polyglot developer and occasional system administrator</p>
-    </div>
+<div class="row">
+  <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+    <p>I'm Rob Dimsdale, a software developer currently based near Boulder, CO, where I work for Oracle in the Tape Storage division (yes that business is still going and is in fact quite healthy). Prior to moving to the US I worked for <a href="http://www.softwire.com">Softwire</a> in London, UK.</p>
+    <p>Whilst my primary focus to date has been Java web applications with a decent dose of systems-administration, I'm interested in all sorts of languages and frameworks. At the low-level I've worked on embedded C projects and hardware and at the opposite end I'm working on projects using scripting languages including Python and MATLAB.</p>
+    <p>For more details, see my <a href="/resume/">resume</a></p>
   </div>
+</div>
 
+<br/>
+
+<?php require('../buttons.php'); ?>
 
 <?php require('../footer.php'); ?>
