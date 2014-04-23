5aa060190df60ea77be9e23e5d032a8bd580e44d
robdimsdale <robert.dimsdale@gmail.com>
2014-04-23 15:44:42 -0600

    Create src and tests directories and add phpunit.

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,5 @@
 <?php
     require_once(__DIR__ . '/vendor/autoload.php');
 
-    require('header-navbar.php');
+    require('src/index.php');
 ?>
-
-<div class="row">
-  <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-    <h1>Rob Dimsdale</h1>
-    <p class="bio-brief">Polyglot developer, occasional system administrator and embracer of open-source technologies.</p>
-  </div>
-</div>
-
-<?php require('buttons.php'); ?>
-
-<?php require('footer.php'); ?>
---
+++ public_html/tests/HeadersHelperTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require '/../src/HeadersHelper.php';
+
+class HeadersHelperTest extends PHPUnit_Framework_TestCase {
+
+    public function testGetHostReturnsCorrectlyWithoutPrecedingWww() {
+        // Arrange
+        $_SERVER['HTTP_HOST'] = "myHost";
+        $headersHelper = new HeadersHelper();
+
+        // Act
+        $host = $headersHelper->getHost();
+
+        // Assert
+        $this->assertEquals("myHost", $host);
+    }
+
+    public function testGetHostReturnsCorrectlyWithPrecedingWww() {
+        // Arrange
+        $_SERVER['HTTP_HOST'] = "www.myHost";
+        $headersHelper = new HeadersHelper();
+
+        // Act
+        $host = $headersHelper->getHost();
+
+        // Assert
+        $this->assertEquals("myHost", $host);
+    }
+
+
+}
+?>
