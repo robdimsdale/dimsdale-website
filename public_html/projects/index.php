2b74f9bf12853ac0dfd4d4b362df1ab18af449f5
Rob <robert.dimsdale@gmail.com>
2014-07-19 17:08:38 -0700

    Add Garage-pi project info.

--- public_html/projects/index.php
+++ public_html/projects/index.php
@@ -8,6 +8,15 @@
 
 <div class="row">
   <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+    <h3>Garage-Pi</h3>
+    <p>A webserver written in golang to display the output of a Raspberry Pi camera module and trigger gpio.</p>
+    <p>A typical use would be to display the interior of a garage and trigger the garage door via gpio.</p>
+    <a href="https://github.com/robdimsdale/garage-pi/">View the GitHub project</a>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
     <h3>Rentchecker</h3>
     <p>Java command-line and android app to check rent owed at <a href="http://myequityapartments.com">myequityapartments.com</a></p>
     <p>Built with Spring Web MVC and Spring for Android, and uses Gradle as the build tool.</p>
