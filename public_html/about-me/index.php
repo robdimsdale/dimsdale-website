35e20679da8b60a51f8c61e34f7b92a02a473ebf
Rob <robert.dimsdale@gmail.com>
2014-07-23 18:25:18 -0700

    Rewrite about-me to be more concise.

--- public_html/about-me/index.php
+++ public_html/about-me/index.php
@@ -8,8 +8,8 @@
 
 <div class="row">
   <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-    <p>I'm Rob Dimsdale, a software developer currently based near San Francisco where I work for <a href="http://www.gopivotal.com/">Pivotal</a> in the <a href="http://www.gopivotal.com/platform-as-a-service/pivotal-cf">Cloud Foundry</a> project. Previously I have worked for <a href="http://www.oracle.com">Oracle</a> in the <a href="http://www.oracle.com/us/products/servers-storage/storage/tape-storage/overview/index.html">Tape Storage</a> division and <a href="http://www.softwire.com">Softwire</a> in London, UK.</p>
-    <p>Whilst my primary focus to date has been Java web applications with a decent dose of systems-administration, I'm interested in all sorts of languages and frameworks. At the low-level I've worked on embedded C projects and hardware and at the opposite end I'm working on projects using interpreted languages including Ruby, Python and MATLAB.</p>
+    <p>I'm Rob Dimsdale, a software developer currently based near San Francisco where I work for <a href="http://www.gopivotal.com/">Pivotal</a> on the <a href="http://www.gopivotal.com/platform-as-a-service/pivotal-cf">Cloud Foundry</a> project. Previously I have worked for <a href="http://www.oracle.com">Oracle</a> in the <a href="http://www.oracle.com/us/products/servers-storage/storage/tape-storage/overview/index.html">Tape Storage</a> division and <a href="http://www.softwire.com">Softwire</a> in London, UK.</p>
+    <p>My primary areas of focus to date have been web applications (mainly Java, some Go and Ruby) and systems-administration, though I'm interested in various languages and technologies from MATLAB and Python all the way down to embedded C projects and audio circuitry.</p>
     <p>For more details, see my <a href="/projects/">projects</a> or view my <a href="/resume/">resume</a></p>
   </div>
 </div>
