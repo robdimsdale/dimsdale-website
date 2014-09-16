c91b336fbaf3f896a06ed82fe4c77f201aea71fa
Rob Dimsdale <robert.dimsdale@gmail.com>
2014-09-15 21:10:22 -0700

    build-resume.sh exits on error

--- build-resume.sh
+++ build-resume.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-resume_dir=public_html/resume
+# Exit immediately on error
+set -e
+
+# Gets the directory that script lives in,
+# irresepctive of where it's executed from.
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+resume_dir=$DIR/public_html/resume
+resume_json_dir=$DIR/resume-json
 resume_theme=classy
 
-cp resume-json/resume.json $resume_dir/robdimsdale-resume.json
-cd resume-json/ && resume export resume.json --theme $resume_theme --format html && cd ..
-cp resume-json/resume.json.html $resume_dir/robdimsdale-resume.html
+RESUME_VERSION=$(resume --version)
+echo "Using resume-cli version: $RESUME_VERSION"
+echo "Using theme: $resume_theme"
+
+cd $resume_json_dir
+cp resume.json $resume_dir/robdimsdale-resume.json
+resume export resume.json --theme $resume_theme --format html
+cp $resume_json_dir/resume.json.html $resume_dir/robdimsdale-resume.html
 
 exit 0
-
