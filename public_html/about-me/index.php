389aa7d5be4bb87a6ba422efd7da4636949854c8
Rob Dimsdale <robert.dimsdale@gmail.com>
2014-09-15 22:31:22 -0700

    Pivotal links use pivotal.io, and shorten about-me lines.

--- public_html/about-me/index.php
+++ public_html/about-me/index.php
@@ -8,8 +8,16 @@
 
 <div class="row">
   <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-    <p>I'm Rob Dimsdale, a software developer currently based near San Francisco where I work for <a href="http://www.gopivotal.com/">Pivotal</a> on the <a href="http://www.gopivotal.com/platform-as-a-service/pivotal-cf">Cloud Foundry</a> project. Previously I have worked for <a href="http://www.oracle.com">Oracle</a> in the <a href="http://www.oracle.com/us/products/servers-storage/storage/tape-storage/overview/index.html">Tape Storage</a> division and <a href="http://www.softwire.com">Softwire</a> in London, UK.</p>
-    <p>My primary areas of focus to date have been web applications (mainly Java, some Go and Ruby) and systems-administration, though I'm interested in various languages and technologies from MATLAB and Python all the way down to embedded C projects and audio circuitry.</p>
+    <p>I'm Rob Dimsdale, a software developer currently based near San Francisco
+where I work for <a href="http://www.pivotal.io/">Pivotal</a> on the
+<a href="http://www.pivotal.io/platform-as-a-service/pivotal-cf">Cloud Foundry</a> project.
+Previously I have worked for <a href="http://www.oracle.com">Oracle</a> in the
+<a href="http://www.oracle.com/us/products/servers-storage/storage/tape-storage/overview/index.html">Tape Storage</a>
+division and <a href="http://www.softwire.com">Softwire</a> in London, UK.</p>
+    <p>My primary areas of focus to date have been web applications
+(mainly Java, some Go and Ruby) and systems-administration,
+though I'm interested in various languages and technologies from MATLAB
+and Python all the way down to embedded C projects and audio circuitry.</p>
     <p>For more details, see my <a href="/projects/">projects</a> or view my <a href="/resume/">resume</a></p>
   </div>
 </div>
