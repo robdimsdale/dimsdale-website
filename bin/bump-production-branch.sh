ad1c648ab6142d4814a82a7bba54527b9e70f82e
Rob Dimsdale <robert.dimsdale@gmail.com>
2014-11-08 12:27:40 -0800

    Use master/develop branch names in bump-production-branch.sh

--- bin/bump-production-branch.sh
+++ bin/bump-production-branch.sh
@@ -7,12 +7,12 @@ CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 read -r -p "You are about to alter the production website. Are you sure? [y/N] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-	set -x
-	git checkout production
-	git rebase master
-	git push
-	git checkout $CUR_BRANCH
+  set -x
+  git checkout master
+  git rebase develop
+  git push
+  git checkout $CUR_BRANCH
 else
-	echo "Exiting."
-	exit 1
+  echo "Exiting."
+  exit 1
 fi
