2fde1da3d91d45e6a6b8fe2d7c4431fc2b242efc
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-01-18 11:19:00 -0800

    Migrate from php to middleman
    - hard-code home url to 'robdimsdale.com'

--- bin/build-resume.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SET resume_dir=app\resume
-SET resume_theme=classy
-
-cd ..\resume-json\ && resume export resume.json --theme %resume_theme% --format html && cd .. && ^
-xcopy /y resume-json\resume.json.html %resume_dir%\robdimsdale-resume.html
--- update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git pull && ^
-git submodule foreach --recursive git submodule sync && ^
-git submodule update --init --recursive
