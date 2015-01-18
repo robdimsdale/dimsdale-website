63776c8010157151f70d57431674304e2aa0d230
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-01-18 11:24:24 -0800

    Use livereload in development
    - activate directory_indexes.

--- config.rb
+++ config.rb
@@ -36,9 +36,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -70,3 +70,5 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :directory_indexes
